db525c373ddd8b319439964503640be45404ed99
Chris Gaona <thebeloved88@sbcglobal.net>
2016-03-07 21:04:37 -0800

    Adds a controller to app to display gem data

--- app.js
+++ app.js
@@ -4,9 +4,13 @@
   var app = angular.module('store', [])
 
   .controller('StoreCtrl', function() {
-
+    this.product = gem;
   });
 
-  
+  var gem = {
+    name: 'Dodecahedron',
+    price: 2.95,
+    description: '....'
+  }
 
 })();
