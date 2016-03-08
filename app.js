e80c075eb715f8480badb27a1b061f6217d16475
Chris Gaona <thebeloved88@sbcglobal.net>
2016-03-07 21:39:35 -0800

    Adds some more directives including ng-repeat & others

--- app.js
+++ app.js
@@ -10,17 +10,37 @@
   var gems = [
     {
       name: 'Dodecahedron',
-      price: 2.95,
+      price: 2,
       description: 'Some gems have hidden qualities beyond their luster, beyond their shine...Dodeca is one of those gems.',
       canPurchase: true,
-      soldOut: false
+      soldOut: false,
+      images: [
+        {
+          full: "image.jpg",
+          thumb: "thumb-image.jpg"
+        },
+        {
+          full: "image2.jpg",
+          thumb: "thumb2-image.jpg"
+        }
+      ]
     },
     {
       name: "Pentagonal Gem",
       price: 5.95,
       description: "...",
       canPurchase: true,
-      soldOut: false
+      soldOut: false,
+      images: [
+        {
+          full: "image.jpg",
+          thumb: "thumb-image.jpg"
+        },
+        {
+          full: "image2.jpg",
+          thumb: "thumb2-image.jpg"
+        }
+      ]
     }
   ];
 
